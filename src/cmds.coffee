e908147a5393b7d4da31c103ebef868d4a53f4ac
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-03-11 13:06:38 +0100

    Music.js

--- src/cmds.coffee
+++ src/cmds.coffee
@@ -28,29 +28,3 @@ exports.avatarlink = (msg, args) ->
         return mention.username + ": unknown!"
   else
     return "Not found!"
-
-exports.playfile = (msg, args) ->
-  if args
-    if args.length > 0
-      if !msg.channel.guild
-        return "This command can only be run on a server!"
-
-      if !msg.member.voiceState.channelID
-        return "You are not in a voice channel!"
-
-      filename = msg.content.substring 6
-      bot.joinVoiceChannel(msg.member.voiceState.channelID).catch (err) ->
-        return "Error joining voice channel: "  + err.messages
-        console.log(err)
-      .then (connection) ->
-        if connection.playing
-          do connection.stopPlaying
-        connection.play filename
-        return 'Now playing **#{filename}**'
-        connection.once "end", () ->
-          bot.createMessage msg.channel.id, 'Finished **#{$filename}**'
-
-    else
-      return "No file specified!"
-  else
-    return "No file specified!"
--- src/main.js
+++ src/main.js
@@ -50,6 +50,38 @@ bot.on("ready", () => {
   funcs.setStatus("NodeJS");
 });
 
+var playCommand = "/play";
+bot.on("messageCreate", (msg) => { // When a message is created
+    if(msg.content.startsWith(playCommand)) { // If the message content starts with "!play "
+        if(msg.content.length <= playCommand.length + 1) { // Check if a filename was specified
+            bot.createMessage(msg.channel.id, "Please specify a filename.");
+            return;
+        }
+        if(!msg.channel.guild) { // Check if the message was sent in a guild
+            bot.createMessage(msg.channel.id, "This command can only be run in a server.");
+            return;
+        }
+        if(!msg.member.voiceState.channelID) { // Check if the user is in a voice channel
+            bot.createMessage(msg.channel.id, "You are not in a voice channel.");
+            return;
+        }
+        var filename = msg.content.substring(playCommand.length + 1); // Get the filename
+        bot.joinVoiceChannel(msg.member.voiceState.channelID).catch((err) => { // Join the user's voice channel
+            bot.createMessage(msg.channel.id, "Error joining voice channel: " + err.message); // Notify the user if there is an error
+            console.log(err); // Log the error
+        }).then((connection) => {
+            if(connection.playing) { // Stop playing if the connection is playing something
+                connection.stopPlaying();
+            }
+            connection.play(filename); // Play the file and notify the user
+            bot.createMessage(msg.channel.id, `Now playing **${filename}**`);
+            connection.once("end", () => {
+                bot.createMessage(msg.channel.id, `Finished **${filename}**`); // Say when the file has finished playing
+            });
+        });
+    }
+});
+
 
 bot.registerCommandAlias("halp", "help"); // Alias !halp to !help
 
